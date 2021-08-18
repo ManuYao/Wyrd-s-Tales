50345e8bcd3fe8a159c295ab9d0ee83a8813d070
Yao <mine.boy230@gmail.com>
2021-08-19 00:15:18 +0200

    UPDATE |  Alpha 0.5.0
    ...Post hall

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
-function logo() {
+function logo(props) {
     return (
         <div className="logo">
+
             <img src="" alt="logo" />
+
+            <div className="titleLogo">
+                <p className="titleT">{props.name}</p>
+            </div>
+
         </div>
     );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
-import Navigation from '../components/Navigation';
+import Naavigation from '../components/Navigation';
 import Logo from '../components/Logo'
+
 class Home extends Component {
     render() {
         return (
+
             <div className = "home">
-                <Navigation />
-                <Logo />
-                <h1>Bienvenue sur L'aventure Wyrd's Tales</h1>
-                <p>Sous titre :)</p>
+            
+                  <Naavigation />  
+                
+                <div className="home_static">
+                  
+                    <Logo name=""/>
+                    <h1>L'aventure Wyrd's Tales</h1>
+                    <p>Sous titre :)</p>
+                    
+                </div>
+
             </div>
         );
     }
